f4575ab00bec3d22c93134a44c5d7e76ff43842c
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-09-01 16:27:01 -0500

    UPDATE

--- MESAS/MASTER/MASTER.ino
+++ MESAS/MASTER/MASTER.ino
@@ -4,53 +4,57 @@
  */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-
-#include "index.h" //Our HTML webpage contents
-
-//SSID and Password of your WiFi router
-const char* ssid = "Circuits4you.com";
-const char* password = "123456789";
-
-ESP8266WebServer server(80); //Server on port 80
-
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
-void handleRoot() {
- String s = MAIN_page; //Read HTML contents
- server.send(200, "text/html", s); //Send web page
-}
-//==============================================================
-//                  SETUP
-//==============================================================
-void setup(void){
-  Serial.begin(9600);
-  
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+#include <ESP8266WiFi.h>
  
-  server.on("/", handleRoot);      //Which routine to handle at root location
-
-  server.begin();                  //Start server
-  Serial.println("HTTP server started");
+//Nombre de red
+const char* nombre = "IZZI-9146";
+ 
+//Password
+const char* password = "F82DC0169146";
+ 
+// Puerto (se suele usar 80 por defecto, pero en algunos casos hay que
+// consultar al proveedor de internet de cual dispone en su conexion)
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(50); // breve espera para que se concrete la conexion
+ 
+  // Conexion al WiFi 
+  WiFi.begin(nombre, password);
+  while (WiFi.status() != WL_CONNECTED)
+    delay(100); //aqui debemos esperar unos instantes
+  Serial.println("Conexión exitosa a WiFi");
+ 
+// Iniciar el servidor
+  server.begin();
+  Serial.println("Iniciado el servidor");
+ 
+  // Escribir la direccion IP
+  Serial.print("el IP es: ");
+  Serial.print(WiFi.localIP());
+  Serial.println("");
 }
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void){
-  server.handleClient();          //Handle client requests
+ 
+void loop() {
+  // Comprobar si se ha conectado algun cliente
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("Nueva conexion");
+   
+    // Escribir las cabeceras HTML
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("");
+    client.println("<!DOCTYPE HTML>");
+  
+    //Escribir el contenido que vamos a visualizar
+    client.println("<html>");
+    client.println("<body>");
+    client.print("Saludos de Robots Argentina");
+    client.println("</body>");
+    client.println("</html>");
+  }
+  delay(1000); // reescribe cada 1 segundo
 }
