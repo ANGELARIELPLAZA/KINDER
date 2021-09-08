00d92aeb097286d16f0ecbdecb421e04fb4507c4
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-09-08 08:41:47 -0500

    update

--- librerias/mesa1/mesa1.ino
+++ librerias/mesa1/mesa1.ino
@@ -7,14 +7,21 @@ const char* mqttServer = "192.168.0.17";    // IP adress Raspberry Pi
 const int mqttPort = 1883;
 const char* mqttUser = "ariel";      // if you don't have MQTT Username, no need input
 const char* mqttPassword = "123";  // if you don't have MQTT Password, no need input
-
+char miArray[6] = {};
+String color;
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+//LED Connections
+const int bluepin = 16; // D0
+const int redpin = 5;    // D1
+const int greenpin = 4; // D2
+
 
 void setup() {
 
   Serial.begin(115200);
-  
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,9 +48,9 @@ void setup() {
 
     }
   }
-
-//  client.publish("esp8266", "Hello Raspberry Pi");
-//  client.subscribe("esp8266");
+   pinMode (redpin, OUTPUT);
+  pinMode (bluepin, OUTPUT);
+  pinMode (greenpin, OUTPUT);
 
 }
 
@@ -55,16 +62,64 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
+    miArray[i] = (char)payload[i];
   }
+  Serial.println();
+
+  Serial.println( miArray);
+  color = miArray;
+
+
+  Serial.println();
+  String hexstring = color ;
+
+  // Get rid of '#' and convert it to integer
+  int number = (int) strtol( &hexstring[1], NULL, 16);
+
+  // Split them up into r, g, b values
+  int r = number >> 16;
+  int g = number >> 8 & 0xFF;
+  int b = number & 0xFF;
+  //  //Saperate Colors are sent through javascript
+  //  String red = server.arg("r");
+  //  String green = server.arg("g");
+  //  String blue = server.arg("b");
+  //  int r = red.toInt();
+  //  int g = green.toInt();
+  //  int b = blue.toInt();
+  //
+  Serial.print("Red:"); Serial.println(r);
+  Serial.print("Green:"); Serial.println(g);
+  Serial.print("Blue:"); Serial.println(b);
+  //
+  //  //PWM Correction 8-bit to 10-bit
+  //    r = r * 4;
+  //    g = g * 4;
+  //    b = b * 4;
+  analogWrite (bluepin, b);
+  analogWrite (greenpin, 255 - g);
+  analogWrite (redpin, 128 - r);
+  //
+  //  //for ULN2003 or Common Cathode RGB Led not needed
+  //  /*
+  //   r = 1024 - r;
+  //   g = 1024 - g;
+  //   b = 1024 - b;
+  //  */
+  //  //ESP supports analogWrite All IOs are PWM
+  //   analogWrite(RedLED,r);
+  //   analogWrite(GreenLED,g);
+  //   analogWrite(BlueLED,b);
 
   Serial.println();
   Serial.println("-----------------------");
 
+
+
 }
 
 void loop() {
-    //client.publish("esp8266", "Hello Raspberry Pi");
-    client.subscribe("mesa1");
-    delay(300);
+  client.subscribe("mesa1");
+  delay(100);
   client.loop();
 }
